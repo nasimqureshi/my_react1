448bcf3ff981f40442b0dd54797e5417898f9571
nasimqureshi <qureshi.nasim@yahoo.com>
2023-02-18 13:43:40 -0600

    commit

--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,5 @@
 // @flow 
-import  { useState } from 'react';
+import  { useState, useEffect } from 'react';
 import BlogList from './BlogList';
 
 
@@ -9,10 +9,26 @@ import BlogList from './BlogList';
         { title: 'Welcome part', body: 'lorem ipsum...', author: 'Qamar', id: 2},
         { title: 'Wev dev top tips', body: 'lorem ipsum...', author: 'Samreen', id: 1},
     ]);
+    const [name, setName] = useState('Nasim Qureshi');
+
+
+    const handleDelete = (id) => {
+       const newBlogs = blogs.filter(blog => blog.id !==id) ;
+       setBlogs(newBlogs);
+    }
+    useEffect(() => {
+      console.log('use effect ran');
+      console.log(name);
+    },[name]);
+
 
     return (
         <div className='home'>
-          <BlogList blogs={blogs}/>   
+          {/* <BlogList blogs={blogs} title="All Blogs!"/>   
+          <BlogList blogs={blogs.filter((blog) => blog.author === "Qamar")} title="Qamr's logs!"/>    */}
+          <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete} />
+          <button onClick={() => setName('Qamar Qureshi')}>Change Name</button>
+          <p>{ name }</p>
         </div>
     );
 };
